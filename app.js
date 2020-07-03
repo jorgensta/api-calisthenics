8502f0630c052e2b29e8b5d726c6f88a1e4c111b
Jørgen Stamnes <jorgenstamnes@Jrgens-MacBook-Pro.local>
2020-07-03 11:49:26 +0200

    Added some dependencies or something

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const app = express();
 
+require("dotenv").config();
+
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 const PORT = 4000;
 
-app.get("/", (req, res, next) => {
-    res.send({ message: "Hallaballa" });
+app.get("/", async (req, res) => {
+    res.send("Hello")
 });
+
+app.get("/secret", async (req, res) => {
+    const intent = await stripe.paymentIntents.create({
+        amount: 1099,
+        currency: "nok",
+        metadata: {
+            integration_check: 'accept_a_payment'
+        }
+    })
+
+    res.json({ client_secret: intent.client_secret })
+})
+
+app.get("/health", (req, res, next) => {
+    res.send({ status: "ok" })
+})
 
 
 app.listen(PORT, () => { console.log(`App listening on port ${PORT}`) });
